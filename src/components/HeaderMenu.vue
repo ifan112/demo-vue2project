0b950de867e523f95397cfe24a628b27f29a4c37
ifan112 <ifan.w112@gmail.com>
2021-06-24 22:58:01 +0800

    支持遍历生成菜单

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -7,21 +7,22 @@
       @select="handleSelect">
 
       <el-menu-item>
-        <img src="../assets/logo.png" height="32px"/>
+        <img src="../assets/logo.png" height="32px" />
       </el-menu-item>
 
-      <el-menu-item index="/dashboard">首页</el-menu-item>
+      <template v-for="(menu, index) in menus">
+        
+        <el-submenu v-if="menu.subMenus && menu.subMenus.length > 0" :key="index">
+          <template slot="title">{{ menu.name }} </template>
+          <el-menu-item v-for="(subMenu, index2) in menu.subMenus" :key="index2" :index="subMenu.path">
+            {{ subMenu.name }}
+          </el-menu-item>
+        </el-submenu>
 
-      <el-menu-item index="/cases">案件列表</el-menu-item>
-
-      <el-menu-item index="/report">绩效报表</el-menu-item>
-
-      <el-menu-item index="/collectors">催收员管理</el-menu-item>
-
-      <el-submenu>
-        <template slot="title">系统管理</template>
-        <el-menu-item index="/system/role">角色管理</el-menu-item>
-      </el-submenu>
+        <el-menu-item v-else :key="index" :index="menu.path">
+          {{ menu.name }}
+        </el-menu-item>
+      </template>
 
       <el-menu-item index="/profile" style="float: right; margin-right: 12px">
           个人中心
@@ -33,24 +34,46 @@
 
 <script>
 export default {
-    name: 'HeaderMenu',
+  name: "HeaderMenu",
 
-    data() {
-        return {
-            activeIndex: "dashboard"
-        }
+  data() {
+    return {
+      activeIndex: "dashboard",
+      menus: [
+        {
+          path: "/dashboard",
+          name: "首页"
+        },
+        {
+          path: "/cases",
+          name: "案件列表"
+        },
+        {
+          path: "/report",
+          name: "绩效报表"
+        },
+        {
+          path: "/collectors",
+          name: "催收员管理"
+        },
+        {
+          path: "",
+          name: "系统管理",
+          subMenus: [
+            {
+              path: "/system/role",
+              name: "角色管理",
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    handleSelect() {
+      console.log("Menu click");
     },
-
-    methods: {
-        handleSelect() {
-            console.log("Menu click")
-        }
-    }
-}
+  },
+};
 </script>
-
-<style>
-
-
-
-</style>
